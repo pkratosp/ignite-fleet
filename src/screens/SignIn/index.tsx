f7760094c82d75911f8e626f1889ec018e962287
pedro <pfaria.sjp@ecori.com.br>
2025-02-06 08:39:02 -0300

    criando porjeto

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { styles } from "./style";
+import { StatusBar } from "expo-status-bar";
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
