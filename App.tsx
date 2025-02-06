ab360d1af23c18e14dd6664f77fdf45a4487a911
pedro <pfaria.sjp@ecori.com.br>
2025-02-06 11:03:05 -0300

    configurando fontes e criando componente de loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { ThemeProvider } from "styled-components/native";
 import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
       <SignIn />
     </ThemeProvider>
   );
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { Container, LoadingIdicator } from "./style";
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadingIdicator />
+    </Container>
+  );
+}
