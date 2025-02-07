0e8d131b5815f8c29903639b5325077cf6a94087
pedro <pfaria.sjp@ecori.com.br>
2025-02-07 11:43:19 -0300

    centralizando textos

--- src/screens/SignIn/style.ts
+++ src/screens/SignIn/style.ts
@@ -11,12 +11,13 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.COLORS.BRAND_LIGHT};
   font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  text-align: center;
 `;
 
 export const Slogan = styled.Text`
   color: ${({ theme }) => theme.COLORS.GRAY_100};
   font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-
+  text-align: center;
   margin-bottom: 32px;
 `;
