da0389c7bc9727eea26bc515d0f33c8cfc068821
Pedro <70226968+pkratosp@users.noreply.github.com>
2025-02-13 10:48:55 +0000

    instalando oauth para autenticação com o google e implementando autenticação

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,47 @@
 import { Container, Slogan, Title } from "./style";
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
 
 export function SignIn() {
+  const [isLoadingAuthenticate, setIsLoadingAuthenticate] = useState(false);
+
+  async function handleSignIn() {
+    try {
+      setIsLoadingAuthenticate(true);
+
+      const response = await GoogleSignin.signIn();
+
+      if (response.data?.idToken) {
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
+        );
+      }
+    } catch (error) {
+      Alert.alert("Entrar", "Não foi possível conectar-se a sua conta google.");
+    } finally {
+      setIsLoadingAuthenticate(false);
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Iginite fleet</Title>
 
       <Slogan>Gestão de uso de veiculos</Slogan>
 
-      <Button title="Entrar com o google" />
+      <Button title="Entrar com o google" isLoading={isLoadingAuthenticate} />
     </Container>
   );
 }
